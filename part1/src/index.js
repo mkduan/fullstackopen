542c6b64dfb44103ff297b3c5d6c03d3052d5f82
Mkduan <masonkbduan@gmail.com>
2020-05-11 00:34:18 -0700

    Exercise 1.6

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
+/*
 const Header = (props) => {
   return(
   <h1>{props.course.name}</h1>
@@ -53,6 +54,73 @@ const App = () => {
       <Header course = {course}/>
       <Content course = {course}/>
       <Total course = {course} />
+    </div>
+  )
+  */
+
+  const Title = (props) => {
+    return(
+    <h1>{props.title}</h1>
+    )
+  }
+
+  const SubTitle = (props) => {
+    return(
+    <h2>{props.title}</h2>
+    )
+  }
+
+  const Button = (props) => {
+    return(
+      <button onClick = {props.handleClick}>
+        {props.text}
+      </button>
+    )
+  }
+
+  const DisplayStat = (props) => {
+    return(
+    <p>{props.statTitle} {props.stat}</p>
+    )
+  }
+
+ const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const pageTitle = "give feedback"
+  const goodText = "good"
+  const neutralText = "neutral"
+  const badText = "bad" 
+  const statTitle = "statistics"
+
+  const incrementGood = () => {
+    console.log("Good count:", good)
+    setGood(good + 1)
+  }
+
+  const incrementNeutral = () => {
+    console.log("Neutral count:", neutral)
+    setNeutral(neutral + 1)
+  }
+
+  const incrementBad = () => {
+    console.log("Bad count:", bad)
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <Title title = {pageTitle}/>
+      <Button handleClick = {incrementGood} text = {goodText}/>
+      <Button handleClick = {incrementNeutral} text = {neutralText}/>
+      <Button handleClick = {incrementBad} text = {badText}/>
+      <SubTitle title = {statTitle}/>
+      <DisplayStat statTitle = {goodText} stat = {good}/>
+      <DisplayStat statTitle = {neutralText} stat = {neutral}/>
+      <DisplayStat statTitle = {badText} stat = {bad}/>
     </div>
   )
 }
