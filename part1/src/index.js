5d092be3e1d08e75943ebf1013c6d6ed4f72ad55
Mkduan <masonkbduan@gmail.com>
2020-07-05 00:25:47 -0700

    Exercise 1.12

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,104 +1,46 @@
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-  const Title = (props) => {
-    return(
-    <h1>{props.title}</h1>
-    )
-  }
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
-  const Button = (props) => {
-    return(
-      <button onClick = {props.handleClick}>
-        {props.text}
-      </button>
-    )
-  }
+const NextButton = (props) => {
+  return (
+    <button onClick = {props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
 
-  const Statistic = (props) => {
-    if(props.text === "positive") {
-      return(
-        <tbody>
-          <tr>
-            <td>{props.text}</td>
-            <td>{props.stat} %</td>
-          </tr>
-        </tbody>
-        )
-     }
-    return(
-      <tbody>
-          <tr>
-            <td>{props.text}</td>
-            <td>{props.stat}</td>
-          </tr>
-        </tbody>
-    )
-  }
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
 
-  const Statistics = (props) => {
-    if((props.good + props.neutral + props.bad) === 0) {
-      return(
-        <div>
-          <Title title = {props.statTitle}/>
-          <p>No feedback given</p>
-        </div>
-      )
+const nextAnecdote = "next anecdote"
+
+  const cycleAnecdotes = () => {
+    console.log("Cycling to next ancedote")
+    if(selected === anecdotes.length-1) {
+      setSelected(0)
+    } else {
+      setSelected(selected + 1)
     }
-    return(
-      <div>
-        <Title title = {props.statTitle}/>
-        <table>
-          <Statistic text = {props.goodText} stat = {props.good}/>
-          <Statistic text = {props.neutralText} stat = {props.neutral}/>
-          <Statistic text = {props.badText} stat = {props.bad}/>
-          <Statistic text = {props.allText} stat = {props.good+props.neutral+props.bad}/>
-          <Statistic text = {props.averageText} stat = {(props.good - props.bad)/(props.good + props.neutral + props.bad)}/>
-          <Statistic text = {props.positiveText} stat = {(props.good)/(props.good + props.neutral + props.bad)}/>
-        </table>
-      </div>
-    )
-  }
-
- const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  const pageTitle = "give feedback"
-  const goodText = "good"
-  const neutralText = "neutral"
-  const badText = "bad" 
-  const statTitle = "statistics"
-  const allText = "all"
-  const averageText = "average"
-  const positiveText = "positive"
-
-  const incrementGood = () => {
-    console.log("Good count:", good)
-    setGood(good + 1)
-  }
-
-  const incrementNeutral = () => {
-    console.log("Neutral count:", neutral)
-    setNeutral(neutral + 1)
-  }
-
-  const incrementBad = () => {
-    console.log("Bad count:", bad)
-    setBad(bad + 1)
   }
 
   return (
     <div>
-      <Title title = {pageTitle}/>
-      <Button handleClick = {incrementGood} text = {goodText}/>
-      <Button handleClick = {incrementNeutral} text = {neutralText}/>
-      <Button handleClick = {incrementBad} text = {badText}/>
-      <Statistics statTitle = {statTitle} goodText = {goodText} good = {good} neutralText = {neutralText} neutral = {neutral} badText = {badText} bad = {bad} allText = {allText} averageText = {averageText} positiveText = {positiveText}/>
+      <p>{props.anecdotes[selected]}</p>
+      <NextButton handleClick = {cycleAnecdotes} text = {nextAnecdote}/>
     </div>
   )
 }
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
