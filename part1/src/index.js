50292addf3ec31b9db84cb0678e9aeb913a8b2e5
Mkduan <masonkbduan@gmail.com>
2020-07-04 21:08:37 -0700

    Exercise 1.10

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,63 +1,6 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
-/*
-const Header = (props) => {
-  return(
-  <h1>{props.course.name}</h1>
-  )
-}
-
-const Part= (props) => {
-  return(
-    <p> {props.part} {props.exercises} </p>
-  )
-}
-
-const Content = (props) => {
-  return(
-    <div>
-    <Part part = {props.course.parts[0].name} exercises = {props.course.parts[0].exercises} />
-    <Part part = {props.course.parts[1].name} exercises = {props.course.parts[1].exercises} />
-    <Part part = {props.course.parts[2].name} exercises = {props.course.parts[2].exercises} />
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return(
-    <p>Number of exercises {props.course.parts[0].exercises+props.course.parts[1].exercises+props.course.parts[2].exercises}</p>
-  )
-}
-
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-
-  return(
-    <div>
-      <Header course = {course}/>
-      <Content course = {course}/>
-      <Total course = {course} />
-    </div>
-  )
-  */
-
   const Title = (props) => {
     return(
     <h1>{props.title}</h1>
@@ -78,15 +21,15 @@ const App = () => {
     )
   }
 
-  const DisplayStat = (props) => {
-    //console.log(props.statTitle, props.stat)
-    if(props.statTitle === "positive") {
+  const Statistic = (props) => {
+    //console.log(props.text, props.stat)
+    if(props.text === "positive") {
       return(
-        <p>{props.statTitle} {props.stat} %</p>
+        <p>{props.text} {props.stat} %</p>
         )
      }
     return(
-    <p>{props.statTitle} {props.stat}</p>
+    <p>{props.text} {props.stat}</p>
     )
   }
 
@@ -102,12 +45,12 @@ const App = () => {
     return(
       <div>
         <Title title = {props.statTitle}/>
-        <DisplayStat statTitle = {props.goodText} stat = {props.good}/>
-        <DisplayStat statTitle = {props.neutralText} stat = {props.neutral}/>
-        <DisplayStat statTitle = {props.badText} stat = {props.bad}/>
-        <DisplayStat statTitle = {props.allText} stat = {props.good+props.neutral+props.bad}/>
-        <DisplayStat statTitle = {props.averageText} stat = {(props.good - props.bad)/(props.good + props.neutral + props.bad)}/>
-        <DisplayStat statTitle = {props.positiveText} stat = {(props.good)/(props.good + props.neutral + props.bad)}/>
+        <Statistic text = {props.goodText} stat = {props.good}/>
+        <Statistic text = {props.neutralText} stat = {props.neutral}/>
+        <Statistic text = {props.badText} stat = {props.bad}/>
+        <Statistic text = {props.allText} stat = {props.good+props.neutral+props.bad}/>
+        <Statistic text = {props.averageText} stat = {(props.good - props.bad)/(props.good + props.neutral + props.bad)}/>
+        <Statistic text = {props.positiveText} stat = {(props.good)/(props.good + props.neutral + props.bad)}/>
       </div>
     )
   }
